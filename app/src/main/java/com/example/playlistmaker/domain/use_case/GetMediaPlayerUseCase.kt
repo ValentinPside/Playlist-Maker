ceed60a3e764aafa94573864fb5f544e705bd447
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2023-09-26 17:27:20 +0300

    Спринт 15

---
+++ app/src/main/java/com/example/playlistmaker/domain/use_case/GetMediaPlayerUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.playlistmaker.domain.use_case
+
+import com.example.playlistmaker.data.creator.CreatorMediaPlayer
+
+class GetMediaPlayerUseCase {
+     fun execute() : CreatorMediaPlayer {
+        return CreatorMediaPlayer
+    }
+}
